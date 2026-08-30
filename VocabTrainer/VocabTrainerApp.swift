1d12ef48fb575b32c702762338eae2b11de1bb85
Malte95 <maltescherenberg@gmx.de>
2026-08-30 18:32:36 +0200

    Create initial Xcode project

---
+++ VocabTrainer/VocabTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocabTrainerApp.swift
+//  VocabTrainer
+//
+//  Created by Malte Scherenberg on 30.08.26.
+//
+
+import SwiftUI
+
+@main
+struct VocabTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VocabTrainerTests/VocabTrainerTests.swift
@@ -0,0 +1,19 @@
+//
+//  VocabTrainerTests.swift
+//  VocabTrainerTests
+//
+//  Created by Malte Scherenberg on 30.08.26.
+//
+
+import Testing
+@testable import VocabTrainer
+
+struct VocabTrainerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
