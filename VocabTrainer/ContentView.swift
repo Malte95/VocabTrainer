d556d228ed12baebb95272e267a1692a568dad93
Malte95 <maltescherenberg@gmx.de>
2026-08-31 12:52:09 +0200

    Customize initial content view

--- VocabTrainer/ContentView.swift
+++ VocabTrainer/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "shippingbox.fill")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello world")
+            Text("Meine Vokabelboxen")
         }
         .padding()
     }
