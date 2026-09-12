f9cd19ac8bb22994d80730a7b05856a68486d8a5
Malte95 <maltescherenberg@gmx.de>
2026-09-12 20:48:58 +0200

    Add practice answer feedback

--- VocabTrainer/PracticeView.swift
+++ VocabTrainer/PracticeView.swift
@@ -11,6 +11,9 @@ struct PracticeView: View {
     let box: VocabBox
     @State private var remainingVocabularies: [Vocabulary] = []
     @State private var remainingAnswers: [Vocabulary] = []
+    @State private var lastIncorrectAnswer: Vocabulary? = nil
+    @State private var lastCorrectAnswer: Vocabulary? = nil
+    @State private var isProcessingAnswer = false
     var body: some View {
         VStack {
             Text(box.name)
@@ -19,12 +22,27 @@ struct PracticeView: View {
                 ForEach(remainingAnswers) { vocabulary in
                     Button(vocabulary.english) {
                         if vocabulary === currentVocabulary {
-                            remainingVocabularies.removeFirst()
-                            remainingAnswers.removeAll {answer in
-                                answer === currentVocabulary}
-                            remainingAnswers.shuffle()
+                            lastCorrectAnswer = vocabulary
+                            lastIncorrectAnswer = nil
+                            isProcessingAnswer = true
+                            Task {
+                                try? await Task.sleep(for: .milliseconds(500))
+                                remainingVocabularies.removeFirst()
+                                remainingAnswers.removeAll {answer in
+                                    answer === currentVocabulary}
+                                remainingAnswers.shuffle()
+                                lastCorrectAnswer = nil
+                                isProcessingAnswer = false
+                            }
+                            
+                        } else {
+                            lastIncorrectAnswer = vocabulary
                         }
                     }
+                    .foregroundStyle(
+                        answerColor(for: vocabulary)
+                    )
+                    .disabled(isProcessingAnswer)
                 }
                 
             }
@@ -33,6 +51,16 @@ struct PracticeView: View {
             let shuffledArray = box.vocabularies.shuffled()
             remainingVocabularies = Array(shuffledArray.prefix(10))
             remainingAnswers = remainingVocabularies.shuffled()
+        }
+    }
+    
+    private func answerColor(for vocabulary: Vocabulary) -> Color {
+        if vocabulary === lastCorrectAnswer {
+           return Color.green
+        } else if vocabulary === lastIncorrectAnswer {
+            return Color.red
+        } else {
+            return Color.accentColor
         }
     }
        
