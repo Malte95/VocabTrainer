fa576f161f46dd9b111adfd3917a661941c1d160
Malte95 <maltescherenberg@gmx.de>
2026-09-14 15:38:12 +0200

    Add training mode tab selection

--- VocabTrainer/TrainingOverviewView.swift
+++ VocabTrainer/TrainingOverviewView.swift
@@ -7,10 +7,49 @@
 
 import SwiftUI
 
+enum TrainingTab {
+    case practice
+    case vocabularyTest
+}
 struct TrainingOverviewView: View {
     let box: VocabBox
+    @State private var selectedTab: TrainingTab = .practice
     var body: some View {
-        Text(box.name)
+        VStack {
+            Text(box.name)
+            HStack {
+                VStack {
+                    Button("Übung"){
+                        selectedTab = .practice
+                    }
+                    .foregroundStyle(selectedTab == .practice
+                                     ? Color.orange
+                                     : Color.primary)
+                    .frame(maxWidth: .infinity)
+                    Rectangle()
+                        .fill(selectedTab == .practice
+                              ? Color.orange
+                              : Color.clear)
+                        .frame(height: 3)
+                }
+                
+                VStack {
+                    Button("Vokabeltest") {
+                        selectedTab = .vocabularyTest
+                    }
+                    .foregroundStyle(selectedTab == .vocabularyTest
+                                     ? Color.orange
+                                     : Color.primary)
+                    .frame(maxWidth: .infinity)
+                    Rectangle()
+                        .fill(selectedTab == .vocabularyTest
+                              ? Color.orange
+                              : Color.clear)
+                        .frame(height: 3)
+                }
+            }
+        }
+        
     }
 }
 
